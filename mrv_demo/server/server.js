392165b33e8597d671f9fc970be5336fdd88668e
jongsiklim <whdtlr1218@gmail.com>
2018-05-25 15:54:06 +0900

    add : electron package

--- mrv_demo/server/server.js
+++ mrv_demo/server/server.js
@@ -1,9 +1,9 @@
-// var express = require('express');
-// var app = express();
-// var path = require('path');
+var express = require('express');
+var path = require('path');
+var app = express();
 
-// app.use('/', express.static(path.join(__dirname, '/')));
-// app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
-// app.get('/', (res, req)=>{res.sendFile('index.html')});
+app.use('/', express.static(path.join(__dirname, '../')));
+app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
+app.get('/', (res, req)=>{res.sendFile('index.html')});
 
-// app.listen(3001, ()=>{console.log('port: 3001 server On!')});
+app.listen(3000, ()=>{console.log('http://127.0.0.1:3000 server on!')});
